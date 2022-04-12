3a86124dd573b903f116049cd7b6cebc86db3fad
9las <nilasschusler@gmail.com>
2022-04-12 15:29:25 +0000

    Replace content of load script with the content of temp script

--- R/01_load.R
+++ R/01_load.R
@@ -1,61 +1,51 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 library("readxl")
-library("bench")
-library("profvis")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-morphometric_data_raw <- read_xls(path = "data/_raw/JEvolBiol_Sensory evolution Poecilimon_Database.xls",
-                                  sheet = "Database")
-physiological_data_raw <- read_xls(path = "data/_raw/JEvolBiol_Sensory evolution Poecilimon_Database.xls",
-                                   sheet = "Database 2",
-                                   skip = 1,
-                                   col_names = FALSE)
+extractor <- function(path = "data/_raw/JEvolBiol_Sensory evolution Poecilimon_Database.xls", 
+                      coords){
+  
+  df_test <- read_xls(path = path, range = coords, sheet = "Database 2")
+  
+  names <- df_test %>% 
+    names()
+  specie <- names[[1]]
+  
+  sex <- names[[2]]
+  
+  indices <- df_test %>% 
+    select(!1) %>% 
+    slice(1) %>% 
+    as.character()
+  
+  data <- df_test %>% 
+    slice(2:n())
+  
+  colnames(data) <- c("hertz", 
+                      indices)
+  
+  data <- data %>% 
+    pivot_longer(cols = !hertz,
+                 names_to = "id",
+                 values_to = "decibel")
+  
+  data %>% 
+    mutate(specie = specie, 
+           sex = sex, 
+           hertz = as.character(hertz))
+  
+}
 
-meta_data_raw <- read_xls(path = "data/_raw/jeb12294-sup-0001-tables1.xls",
-                          skip = 3,
-                          col_names = FALSE)
+places <- c("A2:I18","K2:R18","A20:G36", 
+            "K20:S36", "A38:I54", "K38:S54", "A56:I72")
 
-# Wrangle data ------------------------------------------------------------
-physiological_subtable_coordinates <- tibble(first_row = c(1,
-                                                           1,
-                                                           19,
-                                                           19,
-                                                           37,
-                                                           37,
-                                                           55),
-                                             first_column = c(1,
-                                                              11,
-                                                              1,
-                                                              11,
-                                                              1,
-                                                              11,
-                                                              1),
-                                             last_row = c(17,
-                                                          17,
-                                                          35,
-                                                          35,
-                                                          53,
-                                                          53,
-                                                          71),
-                                             last_column = c(9,
-                                                             18,
-                                                             7,
-                                                             19,
-                                                             9,
-                                                             19,
-                                                             9))
+combiner <- function(coords){
+  map_dfr(.x = coords, .f = ~ extractor(coords = .x))
+  
+}
 
-physiological_data <- physiological_subtable_coordinates %>%
-  pmap_dfr(.f = ~ tidy_subtable_physiological(data = physiological_data_raw,
-                                              first_row = ..1,
-                                              first_column = ..2,
-                                              last_row = ..3,
-                                              last_column = ..4))
-
-# Write data --------------------------------------------------------------
-#write_tsv(x = my_data,
-#          file = "data/01_my_data.tsv")
+combiner(coords = places)
