530d330e681322d64e967fac6e181c172a55ef54
9las <nilasschusler@gmail.com>
2022-04-12 15:31:27 +0000

    Remove temp script

--- R/01_load.R
+++ R/01_load.R
@@ -1,8 +1,6 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 library("readxl")
-library("bench")
-library("profvis")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
