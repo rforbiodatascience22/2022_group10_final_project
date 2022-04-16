2a622b080272ff657e6f283a1f93fb784022e2d6
9las <nilasschusler@gmail.com>
2022-04-16 13:52:53 +0000

    Add comments to loading script and tidy code a bit

--- R/01_load.R
+++ R/01_load.R
@@ -8,6 +8,7 @@ source(file = "R/99_project_functions.R")
 # Load data ---------------------------------------------------------------
 morphometric_data_raw <- read_xls(path = "data/_raw/JEvolBiol_Sensory evolution Poecilimon_Database.xls",
                                   sheet = "Database")
+
 physiological_data_raw <- read_xls(path = "data/_raw/JEvolBiol_Sensory evolution Poecilimon_Database.xls",
                                    sheet = "Database 2",
                                    skip = 1,
@@ -18,66 +19,76 @@ meta_data_raw <- read_xls(path = "data/_raw/jeb12294-sup-0001-tables1.xls",
                           col_names = FALSE)
 
 # Wrangle data ------------------------------------------------------------
-physiological_subtable_coordinates <- tibble(first_row = c(1,
-                                                           1,
-                                                           19,
-                                                           19,
-                                                           37,
-                                                           37,
-                                                           55),
-                                             first_column = c(1,
-                                                              11,
-                                                              1,
-                                                              11,
-                                                              1,
-                                                              11,
-                                                              1),
-                                             last_row = c(17,
-                                                          17,
-                                                          35,
-                                                          35,
-                                                          53,
-                                                          53,
-                                                          71),
-                                             last_column = c(9,
-                                                             18,
-                                                             7,
-                                                             19,
-                                                             9,
-                                                             19,
-                                                             9))
 
-physiological_data <- physiological_subtable_coordinates %>%
+## Wrangle physiological data ---------------------------------------------
+
+# Create table of coordinates of subtables that should be extracted
+physiological_coordinates <- tibble(body_top = c(3, 
+                                                 3,
+                                                 21,
+                                                 21,
+                                                 39,
+                                                 39,
+                                                 57),
+                                    body_bottom = c(17, 
+                                                    17,
+                                                    35,
+                                                    35,
+                                                    53,
+                                                    53,
+                                                    71), 
+                                    body_left = c(1,
+                                                  11,
+                                                  1,
+                                                  11,
+                                                  1,
+                                                  11,
+                                                  1), 
+                                    body_right = c(9, 
+                                                   18,
+                                                   7,
+                                                   19,
+                                                   9,
+                                                   19,
+                                                   9))
+
+# Extract and combine subtables
+physiological_data <- physiological_coordinates %>%
   pmap_dfr(.f = ~ tidy_extract_physiological(data = physiological_data_raw,
-                                              first_row = ..1,
-                                              first_column = ..2,
-                                              last_row = ..3,
-                                              last_column = ..4))
+                                             body_top = ..1,
+                                             body_bottom = ..2,
+                                             body_left = ..3,
+                                             body_right = ..4))
 
-meta_subtable_coordinates <- tibble(first_row = c(2,
-                                                  5,
-                                                  10,
-                                                  14,
-                                                  17,
-                                                  25),
-                                    last_row = c(3,
-                                                 9,
-                                                 13,
-                                                 16,
-                                                 24,
-                                                 28))
 
-meta_data <- meta_subtable_coordinates %>%
+## Wrangle meta data ------------------------------------------------------
+
+# Create table of coordinates of subtables that should be extracted
+meta_coordinates <- tibble(body_top = c(3, 
+                                        6,
+                                        11,
+                                        15,
+                                        18,
+                                        26),
+                           body_bottom = c(3,
+                                           9,
+                                           13,
+                                           16,  
+                                           24,
+                                           28))
+
+# Extract and combine subtables
+meta_data <- meta_coordinates %>%
   pmap_dfr(.f = ~ tidy_extract_meta(data = meta_data_raw,
-                                             first_row = ..1,
-                                             first_column = 1,
-                                             last_row = ..2,
-                                             last_column = 6))
+                                    body_top = ..1,
+                                    body_bottom = ..2))
 
 # Write data --------------------------------------------------------------
 write_tsv(x = morphometric_data_raw,
-          file = "data/01_morphometric_data")
+          file = "data/01_morphometric_data.tsv")
+
 write_tsv(x = physiological_data,
-          file = "data/01_physiological_data")
+          file = "data/01_physiological_data.tsv")
+
 write_tsv(x = meta_data,
-          file = "data/01_meta_data")
+          file = "data/01_meta_data.tsv")
