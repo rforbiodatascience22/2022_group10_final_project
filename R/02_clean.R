ea521a2100ed40c17fc3b2fd70ccd2a757b8aa3b
9las <nilasschusler@gmail.com>
2022-04-16 16:57:53 +0000

    Implement cleaning of physiological data

--- R/02_clean.R
+++ R/02_clean.R
@@ -14,19 +14,39 @@ meta_data <- read_tsv(file = "data/01_meta_data.tsv")
 # Wrangle data ------------------------------------------------------------
 ## Morphometric data ------------------------------------------------------
 # Rename columns
-morphometric_data_clean <- morphometric_data %>%
+morphometric_data <- morphometric_data %>%
   rename_with(.fn = ~ .x %>%
                 str_to_lower() %>%
                 str_replace_all(pattern = "\\s",
                             replacement = "_") %>%
-                str_replace(pattern = fixed("[mm]"),
+                str_replace(pattern = "\\[mm\\]$",
                             replacement = "length")) %>%
   rename(sensillae_count = sensillae)
-  
-morphometric_data_clean <- morphometric_data_clean %>%
+
+# Clean column values
+morphometric_data_clean <- morphometric_data %>%
   mutate(sex = case_when(sex == "m" ~ "male",
                          sex == "f" ~ "female"))
+
+## Physiological data -----------------------------------------------------
+# Clean column values
+physiological_data_clean <- physiological_data %>%
+  mutate(frequency = str_replace(string = frequency,
+                                 pattern = "^(\\d+)(\\s*kHz)?$",
+                                 replacement = "\\1"),
+         cricket_id = str_replace(string = cricket_id,
+                                  pattern = "^Nr.\\s*(\\d+(\\s*(re|li))?)$",
+                                  replacement = "\\1"),
+         auditory_threshold = str_replace(string = auditory_threshold,
+                                          pattern = "^(\\d+)(\\s*dB)?$",
+                                          replacement = "\\1"),
+         sex = str_replace(string = sex,
+                           pattern = "^(male|female)\\s+individuals$",
+                           replacement = "\\1"))
 
 # Write data --------------------------------------------------------------
 write_tsv(x = morphometric_data_clean,
           file = "data/02_morphometric_data_clean.tsv")
+
+write_tsv(x = physiological_data_clean,
+          file = "data/02_physiological_data_clean.tsv")
