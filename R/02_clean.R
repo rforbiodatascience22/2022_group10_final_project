d832def50d0a0609181a83a250a665df76703ac7
9las <nilasschusler@gmail.com>
2022-04-20 09:51:40 +0000

    Revert clean script for real this time

--- R/02_clean.R
+++ R/02_clean.R
@@ -1,3 +1,48 @@
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+library("sp")
+library("lubridate")
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
+# Load data ---------------------------------------------------------------
+morphometric_data <- read_tsv(file = "data/01_morphometric_data.tsv",
+                              na = "",
+                              col_types = cols(.default = "c"))
+
+physiological_data <- read_tsv(file = "data/01_physiological_data.tsv",
+                               na = "",
+                               col_types = cols(.default = "c"))
+
+meta_data <- read_tsv(file = "data/01_meta_data.tsv",
+                      na = "",
+                      col_types = cols(.default = "c"))
+
+# Wrangle data ------------------------------------------------------------
+## Morphometric data ------------------------------------------------------
+# Rename columns
+morphometric_data_clean <- morphometric_data %>%
+  rename_with(.fn = ~ .x %>%
+                str_to_lower() %>%
+                str_replace_all(pattern = "\\s",
+                                replacement = "_") %>%
+                str_replace(pattern = "\\[mm\\]$",
+                            replacement = "length")) %>%
+  rename(sensillae_count = sensillae) %>%
+  
+  # Clean column values
+  mutate(across(.cols = everything(),
+                .fns = str_trim),
+         sex = case_when(sex == "m" ~ "male",
+                         sex == "f" ~ "female"),
+         genus_species = {str_sub(string = genus,
+                                  start = 1,
+                                  end = 1) %>%
+             str_c(species,
+                   sep = ". ")})
+
+## Physiological data -----------------------------------------------------
 # Clean column values
 physiological_data_clean <- physiological_data %>%
   mutate(across(.cols = everything(),
@@ -21,7 +66,6 @@ physiological_data_clean <- physiological_data %>%
                            replacement = "\\1"),
          genus = case_when(genus == "P." ~ "Poecilimon",
                            genus == "I." ~ "Isophya"))
-
 
 ## Meta data --------------------------------------------------------------
 # Rename columns
