97e46ae3eff4287a5f85be3be0e99c4b22edf731
MikkelWolfhagen <s184240@student.dtu.dk>
2022-04-20 08:07:28 +0000

    Renames analysis ii to what it does

--- R/05_analysis_ii_Recreate_Fig_8.R
+++ R/05_analysis_ii_Recreate_Fig_8.R
@@ -8,13 +8,13 @@ source(file = "R/99_project_functions.R")
 my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+
 
 # Model data --------------------------------------------------------------
-my_data_clean_aug %>% ...
+
 
 # Visualise data ----------------------------------------------------------
-my_data_clean_aug %>% ...
+
 
 # Write data --------------------------------------------------------------
 write_tsv(...)
