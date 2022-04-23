035d900ef94fd994128425e6a99ad2f1b2a2ce0e
MikkelWolfhagen <s184240@student.dtu.dk>
2022-04-23 10:34:48 +0000

    Mess around with data

--- R/05_analysis_ii_Recreate_Fig_8.R
+++ R/05_analysis_ii_Recreate_Fig_8.R
@@ -5,15 +5,39 @@ library("tidyverse")
 source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-phys_data <- read_tsv(file = "data/01_physiological_data.tsv")
+phys_data <- read_tsv(file = "data/02_physiological_data_clean.tsv")
+
+view(phys_data)
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug
-View(morphometric_data_clean)
+
+#phys_data <- 
+phys_data %>% 
+  view(cricket_id)
+
+plot1 <- phys_data %>% 
+  group_by(sex) %>% 
+  filter(species == "ornatus") %>% 
+  
+
+plot2 <- phys_data %>% 
+  filter(sex == "female") %>% 
+  filter(species == "ornatus")
+
+plot3 <- phys_data %>% 
+  filter(sex == "male") %>% 
+  filter(species == "ornatus")
 # Model data --------------------------------------------------------------
 
 
 # Visualise data ----------------------------------------------------------
+ggplot(data = plot1,
+       mapping = aes(x = frequency,
+                     y = auditory_threshold,
+                     color = sex)) +
+ # geom_line(data = plot2) +
+  geom_line(data = plot3) +
+  geom_point()
 
 
 # Write data --------------------------------------------------------------
