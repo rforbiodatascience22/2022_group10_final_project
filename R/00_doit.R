1cfd2b0af5e62ff62689902d1a23be91467bf913
Zchafuz-Real <s220257@dtu.dk>
2022-05-05 13:36:22 +0000

    Adding PCA to do it script

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,5 +2,6 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/04_analysis_recreate_morphometric_figures.R")
+source(file = "R/05_analysis_PCA_kmeans.R")
 #source(file = "R/03_augment.R")
 #source(file = "R/04_analysis_i.R")
